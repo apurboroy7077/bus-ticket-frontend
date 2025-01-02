794d67e202b6c630c7b857bdeb56813cf07d50f9
Apurbo Roy <apurboroy7077@gmail.com>
2025-01-02 19:28:07 +0600

    production mode

--- src/data/environmentVariables.ts
+++ src/data/environmentVariables.ts
@@ -1,5 +1,5 @@
 type runningEnvironmentType = "PRODUCTION" | "DEVELOPMENT";
-const runningEnvironment = "DEVELOPMENT" as runningEnvironmentType;
+const runningEnvironment = "PRODUCTION" as runningEnvironmentType;
 const localHostBackendAddress = "http://localhost:5006";
 const onlineBackendAddress = "https://bus-ticket-backend-zeta.vercel.app";
 export const backendAddress =
